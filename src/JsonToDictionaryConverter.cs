37be4e3a1daa406e564c54dc773e24a1318478a9
Tom De Smet <tdesmet@gmail.com>
2015-03-24 15:22:51 +0100

    Allow opening, saving, and creating of new templates and test data. Allow json as data.

---
+++ src/JsonToDictionaryConverter.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
+
+namespace template_designer
+{
+    public class JsonToDictionaryConverter
+    {
+        public static Dictionary<string, object> DeserializeJsonToDictionary(string json)
+        {
+
+            var jObject = JObject.Parse(json);
+
+            var dict = ParseObject(jObject);
+
+
+            return dict;
+        }
+
+        private static Dictionary<string, object> ParseObject(JObject jObject)
+        {
+            var dict = new Dictionary<string, object>();
+            foreach (var property in jObject.Properties())
+            {
+                if (property.Value.Type == JTokenType.Array)
+                {
+                    dict.Add(property.Name, ParseArray(property.Value.ToObject<JArray>()));
+                }
+                else if (property.Value.Type == JTokenType.Object)
+                {
+                    dict.Add(property.Name, ParseObject(property.Value.ToObject<JObject>()));
+                }
+                else
+                {
+                    dict.Add(property.Name, property.Value.ToString());
+                }
+
+
+            }
+            return dict;
+        }
+
+        private static List<object> ParseArray(JArray value)
+        {
+            var list = new List<object>();
+            foreach (var child in value.Children())
+            {
+                if (child.Type == JTokenType.Array)
+                {
+                    list.Add(ParseArray(child.ToObject<JArray>()));
+                }
+                else if (child.Type == JTokenType.Object)
+                {
+                    list.Add(ParseObject(child.ToObject<JObject>()));
+                }
+                else
+                {
+                    list.Add(child.ToString());
+                }
+            }
+            return list;
+        }
+    }
+}
--- src/MainWindow.xaml.cs
+++ src/MainWindow.xaml.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Windows;
+using System.Windows.Navigation;
 using DotLiquid;
+using ICSharpCode.AvalonEdit.Highlighting;
+using Microsoft.Win32;
 
 namespace template_designer
 {
@@ -10,78 +14,164 @@ namespace template_designer
     /// </summary>
     public partial class MainWindow : Window
     {
-        private dynamic _data;
+        private string _currentTemplateFileName;
+        private string _currentDataFileName;
 
         public MainWindow()
         {
             InitializeComponent();
-            GenerateTestData();
-            TemplateTextEditor.TextChanged += (sender, args) => RenderTemplate(TemplateTextEditor.Text);
-            TemplateTextEditor.Text = Properties.Resources.Digest;
+            TemplateTextEditor.TextChanged += (sender, args) => RenderTemplate(TemplateTextEditor.Text, DataTextEditor.Text);
+            DataTextEditor.TextChanged += (sender, args) => RenderTemplate(TemplateTextEditor.Text, DataTextEditor.Text);
             TemplateTextEditor.WordWrap = true;
             TemplateTextEditor.Options.HighlightCurrentLine = true;
             TemplateTextEditor.Options.IndentationSize = 2;
         }
 
-        private void GenerateTestData()
-        {
-            var notification1 = new
-            {
-                type = "Answer",
-                date = DateTime.Now,
-                title = "Alignment of the Series-H Oil process pumps",
-                highlight =
-                    "Here is the answer short test of about xxx characters. Here is the answer short test of about xxx characters. Here is the answer short test of about xxx characters. Here is the answer short test of about . Here is the answer short test of about xxx characters",
-                author = new
-                {
-                    fullName = "Marc Vermeulen",
-                    link = "https=//mondale.n365.com/people"
-                }
-            };
-            var notification2 = new
-            {
-                type = "Comment",
-                date = DateTime.Now,
-                title = "Alignment of the Series-H Oil process pumps",
-                highlight =
-                    "Here is the answer short test of about xxx characters. Here is the answer short test of about xxx characters. Here is the answer short test of about xxx characters. Here is the answer short test of about . Here is the answer short test of about xxx characters",
-                author = new
-                {
-                    fullName = "Marc Vermeulen",
-                    link = "https=//mondale.n365.com/people"
-                }
-            };
-            var notifications = new List<dynamic>
-            {
-                notification1,
-                notification2
-            }.ToArray();
-            _data = new
-            {
-                tenant = new
-                {
-                    name = "microsoft"
-                },
-                notifications = notifications
-            };
-        }
-
-        private void RenderTemplate(string text)
+        private void RenderTemplate(string text, string data)
         {
             try
             {
                 var template = DotLiquid.Template.Parse(text);
-                var rendered = template.Render(Hash.FromAnonymousObject(_data));
+                Dictionary<string, object> obj = JsonToDictionaryConverter.DeserializeJsonToDictionary(data);
+                dynamic parameters = Hash.FromDictionary(obj);
+                var rendered = template.Render(parameters);
                 PreviewWindow.NavigateToString(rendered);
             }
-            catch
+            catch (Exception exception)
             {
+                
             }
         }
 
-        private void PreviewWindow_Navigating(object sender, System.Windows.Navigation.NavigatingCancelEventArgs e)
+        private void PreviewWindow_Navigating(object sender, NavigatingCancelEventArgs e)
         {
             if (e.Uri != null) e.Cancel = true;
+        }
+
+        private void OpenTemplateButton_Click(object sender, RoutedEventArgs e)
+        {
+            AskToSaveTemplate();
+            var dialog = new OpenFileDialog
+            {
+                CheckFileExists = true
+            };
+            var result = dialog.ShowDialog();
+            if (!result.HasValue || !result.Value) return;
+            _currentTemplateFileName = dialog.FileName;
+            TemplateTextEditor.Load(dialog.FileName);
+            TemplateTextEditor.SyntaxHighlighting = HighlightingManager.Instance.GetDefinitionByExtension(Path.GetExtension(_currentTemplateFileName));
+        }
+
+        private void NewTemplateButton_Click(object sender, RoutedEventArgs e)
+        {
+            AskToSaveTemplate();
+
+            _currentTemplateFileName = null;
+            TemplateTextEditor.Clear();
+        }
+
+        private void AskToSaveTemplate()
+        {
+            if (!TemplateTextEditor.IsModified) return;
+            var result = MessageBox.Show(
+                "Do you want to save your changes?",
+                "Template modified",
+                MessageBoxButton.YesNo,
+                MessageBoxImage.Question);
+
+            if (result == MessageBoxResult.Yes)
+            {
+                SaveTemplate();
+            }
+        }
+
+        private void SaveTemplateButton_Click(object sender, RoutedEventArgs e)
+        {
+            SaveTemplate();
+        }
+
+        private void SaveTemplate()
+        {
+            if (_currentTemplateFileName == null)
+            {
+                var dialog = new SaveFileDialog {DefaultExt = ".html"};
+                if (dialog.ShowDialog() ?? false)
+                {
+                    _currentTemplateFileName = dialog.FileName;
+                }
+                else
+                {
+                    return;
+                }
+            }
+            TemplateTextEditor.Save(_currentTemplateFileName);
+        }
+
+
+
+        private void NewDataButton_Click(object sender, RoutedEventArgs e)
+        {
+            AskToSaveData();
+
+            _currentDataFileName = null;
+            DataTextEditor.Clear();
+        }
+
+        private void AskToSaveData()
+        {
+            if (!DataTextEditor.IsModified) return;
+            var result = MessageBox.Show(
+                "Do you want to save your changes?",
+                "Data modified",
+                MessageBoxButton.YesNo,
+                MessageBoxImage.Question);
+
+            if (result == MessageBoxResult.Yes)
+            {
+                SaveData();
+            }
+        }
+
+        private void OpenDataButton_Click(object sender, RoutedEventArgs e)
+        {
+            AskToSaveData();
+            var dialog = new OpenFileDialog
+            {
+                CheckFileExists = true
+            };
+            var result = dialog.ShowDialog();
+            if (!result.HasValue || !result.Value) return;
+            _currentDataFileName = dialog.FileName;
+            DataTextEditor.Load(dialog.FileName);
+            DataTextEditor.SyntaxHighlighting = HighlightingManager.Instance.GetDefinitionByExtension(Path.GetExtension(_currentDataFileName));
+        }
+
+        private void SaveDataButton_Click(object sender, RoutedEventArgs e)
+        {
+            SaveData();
+        }
+
+        private void SaveData()
+        {
+            if (_currentDataFileName == null)
+            {
+                var dialog = new SaveFileDialog {DefaultExt = ".json"};
+                if (dialog.ShowDialog() ?? false)
+                {
+                    _currentDataFileName = dialog.FileName;
+                }
+                else
+                {
+                    return;
+                }
+            }
+            DataTextEditor.Save(_currentDataFileName);
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            AskToSaveTemplate();
+            AskToSaveData();
         }
     }
 }
